5bef901097090c3d200094d07dd7962fd41645c9
Hieu Pham <hieu.pham@anfin.vn>
2022-10-03 09:13:24 +0700

    Relocate markdown.dart

--- super_editor/lib/super_editor.dart
+++ super_editor/lib/super_editor.dart
@@ -48,7 +48,6 @@ export 'src/infrastructure/_logging.dart';
 export 'src/infrastructure/attributed_text_styles.dart';
 export 'src/infrastructure/composable_text.dart';
 export 'src/infrastructure/focus.dart';
-export 'src/infrastructure/super_ignore_pointer.dart';
 export 'src/infrastructure/ime_input_owner.dart';
 export 'src/infrastructure/keyboard.dart';
 export 'src/infrastructure/multi_tap_gesture.dart';
@@ -58,6 +57,9 @@ export 'src/infrastructure/platforms/mobile_documents.dart';
 export 'src/infrastructure/scrolling_diagnostics/scrolling_diagnostics.dart';
 export 'src/infrastructure/strings.dart';
 export 'src/infrastructure/super_textfield/super_textfield.dart';
+
+export 'src/infrastructure/super_ignore_pointer.dart';
+export 'src/markdown/markdown.dart';
 
 // Super Reader
 export 'src/super_reader/read_only_document_android_touch_interactor.dart';
