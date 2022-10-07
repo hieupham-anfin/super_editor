f0175b71f6c8604def61417772e98982506e4608
Hieu Pham <hieu.pham@anfin.vn>
2022-10-07 17:26:29 +0700

    Add parser

---
+++ super_editor/lib/src/parser/parser.dart
@@ -0,0 +1,78 @@
+import 'package:super_editor/super_editor.dart';
+
+const pType = 'p';
+const loType = 'lo';
+const luType = 'lu';
+
+extension DocumentParser on MutableDocument {
+  List<Map<String, dynamic>> toJson() {
+    final list = <Map<String, dynamic>>[];
+
+    this.nodes.forEach((n) {
+      if (n is TextNode) {
+        final att = n.text;
+        final text = att.text;
+        final style = att.spans.markers.map((m) => m.toJson()).toList();
+        if (n is ParagraphNode) {
+          list.add({
+            'type': pType,
+            'text': text,
+            'style': style,
+          });
+        } else if (n is ListItemNode) {
+          if (n.type == ListItemType.ordered) {
+            list.add({
+              'type': loType,
+              'text': text,
+              'style': style,
+            });
+          } else {
+            list.add({
+              'type': luType,
+              'text': text,
+              'style': style,
+            });
+          }
+        }
+      }
+    });
+    return list;
+  }
+
+  static MutableDocument fromJson(List<Map<String, dynamic>> list) {
+    final nodes = <DocumentNode>[];
+
+    list.forEach((l) {
+      final type = l['type'] ?? pType;
+
+      final text = l['text'] ?? '';
+      final style = ((l['style'] ?? []) as List).cast<Map>();
+      final markers = style
+          .map((s) => SpanMarker.fromJson(s as Map<String, dynamic>))
+          .toList();
+      final att = AttributedText(
+        text: text,
+        spans: AttributedSpans(attributions: markers),
+      );
+
+      if (type == pType) {
+        nodes.add(ParagraphNode(
+          id: DocumentEditor.createNodeId(),
+          text: att,
+        ));
+      } else if (type == loType) {
+        nodes.add(ListItemNode.ordered(
+          id: DocumentEditor.createNodeId(),
+          text: att,
+        ));
+      } else if (type == luType) {
+        nodes.add(ListItemNode.unordered(
+          id: DocumentEditor.createNodeId(),
+          text: att,
+        ));
+      }
+    });
+
+    return MutableDocument(nodes: nodes);
+  }
+}
---
+++ super_editor/test/parser.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:super_editor/src/parser/parser.dart';
+import 'package:super_editor/super_editor.dart';
+
+final _testDoc = MutableDocument(nodes: [
+  ParagraphNode(
+    id: DocumentEditor.createNodeId(),
+    text: AttributedText(
+      text: 'Ready-made solutions 📦',
+    ),
+  ),
+  ListItemNode.unordered(
+    id: DocumentEditor.createNodeId(),
+    text: AttributedText(
+      text:
+          'SuperEditor is a ready-made, configurable document editing experience.',
+      spans: AttributedSpans(attributions: [
+        SpanMarker(
+          attribution: boldAttribution,
+          offset: 0,
+          markerType: SpanMarkerType.start,
+        ),
+        SpanMarker(
+          attribution: boldAttribution,
+          offset: 10,
+          markerType: SpanMarkerType.end,
+        ),
+      ]),
+    ),
+  ),
+  ListItemNode.ordered(
+    id: DocumentEditor.createNodeId(),
+    text: AttributedText(
+      text: 'SuperTextField is a ready-made, configurable text field.',
+    ),
+  ),
+]);
+
+void main() {
+  test('', () async {
+    final list = _testDoc.toJson();
+    print(list);
+    final doc = DocumentParser.fromJson(list);
+    print('@@@@@');
+    print(doc.nodes[0] is ParagraphNode);
+    print(doc.nodes[1] is ListItemNode);
+    print(doc.nodes[2] is ListItemNode);
+  });
+}
